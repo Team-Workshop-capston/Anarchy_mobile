9ee0fbc03b8be5e6c675294ab0c1c21a0c81ab3c
dngud9701 <201611136@sangmyung.kr>
2021-08-13 15:30:12 +0900

    17차 복구

--- Anarchy_mobile/Assets/Scripts/3_Play_Script/MoveUnit.cs
+++ Anarchy_mobile/Assets/Scripts/3_Play_Script/MoveUnit.cs
@@ -9,6 +9,7 @@ public class MoveUnit : MonoBehaviour
     public int cost;
     bool isMaster;
     Transform[] area;
+    bool[] isEmpty;
 
     private void Start()
     {
@@ -20,13 +21,28 @@ public class MoveUnit : MonoBehaviour
         if(isMaster)
         {
             area = pairTile.P1_unitArea;
+            isEmpty = pairTile.isP1_unitArea;
             CentralProcessor.Instance.currentUnit.currentTile.isP1_unitArea[CentralProcessor.Instance.currentUnit.myNum] = false;
         }
         else
         {
             area = pairTile.P2_unitArea;
+            isEmpty = pairTile.isP2_unitArea;
             CentralProcessor.Instance.currentUnit.currentTile.isP2_unitArea[CentralProcessor.Instance.currentUnit.myNum] = false;
         }
-        CentralProcessor.Instance.currentUnit.transform.position = area[CentralProcessor.Instance.currentUnit.myNum].position;
+        for(int i = 0; i < 3; i++)
+        {
+            if(isEmpty[i] == false)
+            {
+                isEmpty[i] = true;
+                CentralProcessor.Instance.currentUnit.transform.position = area[i].position;
+                CentralProcessor.Instance.currentUnit.currentTile = pairTile;
+                CentralProcessor.Instance.currentUnit.activeCost -= cost;
+                CentralProcessor.Instance.currentUnit = null;
+                CentralProcessor.Instance.uIManager.SetIdleState();
+                CentralProcessor.Instance.uIManager.UISetActiveTrue();
+                return;
+            }
+        }
     }
 }
