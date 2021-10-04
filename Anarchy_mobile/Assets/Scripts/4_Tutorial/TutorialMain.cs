cf851e0dfc9c7213e3d843470e89ce4cb1d6d08e
Chanyoung_Jeong <rndrnal@naver.com>
2021-10-04 18:39:00 +0900

    JCY
    튜토리얼 기본사항 완료

--- Anarchy_mobile/Assets/Scripts/4_Tutorial/TutorialMain.cs
+++ Anarchy_mobile/Assets/Scripts/4_Tutorial/TutorialMain.cs
@@ -7,17 +7,24 @@ using UnityEngine.EventSystems;
 public class TutorialMain : MonoBehaviour
 {
     public Text TutorialText;
+    public Text Money;
     public int ClickNum = -3;
     public GameObject[] Units;
     public GameObject[] UIs;
     public GameObject Building;
+    public Transform CameraView;
     string BtnName;
-    public bool UnitBtn, Unit2Btn, Unit3Btn, BuildBtn, Build1Btn, MinimapBtn, Minimap2Btn, Minimap3Btn, TurnNextBtn, SettingBtn, Unit3DBtn, MvBtn, AtkBtn, MvokBtn, LocateBtn = false;
+    public bool UnitBtn, Unit2Btn, Unit3Btn, BuildBtn, Build1Btn, MinimapBtn, Minimap2Btn, Minimap3Btn, TurnNextBtn, SettingBtn, Unit3DBtn, MvBtn, AtkBtn, MvokBtn, LocateBtn, Eunit3DBtn = false;
     public bool ButtonLimit = false;
+    public bool UButtonLimit = false;
+    public bool BButtonLimit = false;
     public Color panelColor;
     public bool Unit2nd = false;
     public bool UnitLimit = false;
     public bool BtnClicked = false;
+    public bool UnitSecond = false;
+    public bool TurnNext = false;
+    public bool Dicision = false;
 
 
     void Start()
@@ -45,33 +52,33 @@ public class TutorialMain : MonoBehaviour
                     ClickNum += 1;
                     break;
                 case -1:
-                    TutorialText.text = "이제부터 인터페이스에 대해 설명해 드리겠습니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "이제부터 인터페이스에 대해 설명해 드리겠습니다.";
                     ClickNum += 1;
                     break;
                 case 0:
                     UIs[0].transform.SetAsLastSibling();
-                    TutorialText.text = "먼저 세력 마크와 현재 턴 표시, 턴 타이머입니다. 이는 여러분의 세력이 무엇인지를, 현재 턴 수가 얼마인지, 턴이 얼마나 남았는지를 표시합니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "먼저 세력 마크와 현재 턴 표시, 턴 타이머입니다. 이는 여러분의 세력이 무엇인지를, 현재 턴 수가 얼마인지, 턴이 얼마나 남았는지를 표시합니다.";
                     ClickNum += 1;
                     break;
                 case 1:
                     UIs[9].transform.SetAsLastSibling();
                     UIs[10].transform.SetAsLastSibling();
                     UIs[1].transform.SetAsLastSibling();
-                    TutorialText.text = "잔액창입니다. 여러분의 돈이 얼마나 남았는지 표시합니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "잔액창입니다. 여러분의 돈이 얼마나 남았는지 표시합니다.";
                     ClickNum += 2;
                     break;
                 case 2:
                     UIs[9].transform.SetAsLastSibling();
                     UIs[10].transform.SetAsLastSibling();
                     UIs[2].transform.SetAsLastSibling();
-                    TutorialText.text = "턴타이머입니다. 여러분의 턴이 얼마나 남았는지 표시합니다. 턴의 제한시간은 3분입니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "턴타이머입니다. 여러분의 턴이 얼마나 남았는지 표시합니다. 턴의 제한시간은 3분입니다.";
                     ClickNum += 1;
                     break;
                 case 3:
                     UIs[9].transform.SetAsLastSibling();
                     UIs[10].transform.SetAsLastSibling();
                     UIs[3].transform.SetAsLastSibling();
-                    TutorialText.text = "유닛 생산 버튼입니다. 여러분은 이 버튼을 눌러 새로운 유닛을 만들 수 있습니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "유닛 생산 버튼입니다. 여러분은 이 버튼을 눌러 새로운 유닛을 만들 수 있습니다.";
                     ClickNum += 1;
                     break;
                 case 4:
@@ -79,35 +86,35 @@ public class TutorialMain : MonoBehaviour
                     UIs[10].transform.SetAsLastSibling();
                     UIs[10].transform.SetAsLastSibling();
                     UIs[4].transform.SetAsLastSibling();
-                    TutorialText.text = "건물 건설 버튼입니다. 여러분은 이 버튼을 눌러 새로운 건물을 만들 수 있습니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "건물 건설 버튼입니다. 여러분은 이 버튼을 눌러 새로운 건물을 만들 수 있습니다.";
                     ClickNum += 1;
                     break;
                 case 5:
                     UIs[9].transform.SetAsLastSibling();
                     UIs[10].transform.SetAsLastSibling();
                     UIs[5].transform.SetAsLastSibling();
-                    TutorialText.text = "미니맵입니다. 여러분은 이 미니맵을 통해 화면을 이동하고 유닛의 위치를 알 수 있습니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "미니맵입니다. 여러분은 이 미니맵을 통해 화면을 이동하고 유닛의 위치를 알 수 있습니다.";
                     ClickNum += 1;
                     break;
                 case 6:
                     UIs[9].transform.SetAsLastSibling();
                     UIs[10].transform.SetAsLastSibling();
                     UIs[6].transform.SetAsLastSibling();
-                    TutorialText.text = "턴 종료입니다. 여러분은 이 버튼을 눌러 상대에게 턴을 넘길 수 있습니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "턴 종료입니다. 여러분은 이 버튼을 눌러 상대에게 턴을 넘길 수 있습니다.";
                     ClickNum += 1;
                     break;
                 case 7:
                     UIs[9].transform.SetAsLastSibling();
                     UIs[10].transform.SetAsLastSibling();
                     UIs[7].transform.SetAsLastSibling();
-                    TutorialText.text = "설정 버튼입니다. 여러분은 이 버튼을 눌러 설정을 조절할 수 있습니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "설정 버튼입니다. 여러분은 이 버튼을 눌러 설정을 조절할 수 있습니다.";
                     ClickNum += 1;
                     break;
                 case 8:
                     UIs[9].transform.SetAsLastSibling();
                     UIs[10].transform.SetAsLastSibling();
                     UIs[8].transform.SetAsLastSibling();
-                    TutorialText.text = "게임 종료 버튼입니다. 여러분은 이 버튼을 눌러 게임을 종료할 수 있습니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "게임 종료 버튼입니다. 여러분은 이 버튼을 눌러 게임을 종료할 수 있습니다.";
                     ClickNum += 1;
                     break;
                 case 9:
@@ -116,19 +123,21 @@ public class TutorialMain : MonoBehaviour
                     panelColor = UIs[9].GetComponent<Image>().color;
                     UIs[9].GetComponent<Image>().color = new Color(panelColor.r, panelColor.g, panelColor.b, 0f);
                     Units[0].SetActive(true);
-                    TutorialText.text = "이것이 여러분이 조종할 유닛입니다. 여러분은 이 유닛들을 통해 구역을 점령하고 상대와 전투를 벌여야 합니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "이것이 여러분이 조종할 유닛입니다. 여러분은 이 유닛들을 통해 구역을 점령하고 상대와 전투를 벌여야 합니다.";
                     ClickNum += 1;
                     break;
                 case 10:
-                    TutorialText.text = "유닛에는 3가지 종류가 있습니다. 이 유닛은 근접 유닛입니다. 방어에 특화된 유닛으로 방어력이 높고, 공격할 때 자신의 전투력이 더 낮다면 피해를 입습니다.\n\n진행하려면 클릭하세요.";
+                    TutorialText.text = "유닛에는 3가지 종류가 있습니다. 이 유닛은 근접 유닛입니다. 방어에 특화된 유닛으로 방어력이 높고, 공격할 때 자신의 전투력이 더 낮다면 피해를 입습니다.";
                     ClickNum += 1;
                     break;
                 case 11:
                     ButtonLimit = true;
+                    UButtonLimit = true;
                     TutorialText.text = "유닛 생산 버튼을 클릭하세요.";
                     UIs[3].transform.SetAsLastSibling();
                     break;
                 case 13:
+                    UButtonLimit = true;
                     TutorialText.text = "다시 한번 유닛 생산 버튼을 클릭하세요.";
                     UIs[3].transform.SetAsLastSibling();
                     Unit2nd = true;
@@ -142,6 +151,8 @@ public class TutorialMain : MonoBehaviour
                     ClickNum++;
                     break;
                 case 17:
+                    BButtonLimit = true;
+                    Debug.Log(BButtonLimit);
                     TutorialText.text = "이제 빌드 버튼을 눌러보세요";
                     UIs[4].transform.SetAsLastSibling();
                     ClickNum++;
@@ -160,12 +171,43 @@ public class TutorialMain : MonoBehaviour
                     TutorialText.text = "이제 윗쪽 버튼인 이동 버튼을 눌러보세요.";
                     ClickNum++;
                     break;
-                    /*
-                    case 24:
-                    panelColor = UIs[17].GetComponent<Image>().color;
-                    UIs[17].GetComponent<Image>().color = new Color(0, 0, 255, 255f);
+                case 24:
+                    ClickNum++;
                     break;
-                    */
+                case 26:
+                    TutorialText.text = "이제 전투 방식을 알려드리겠습니다.";
+                    Units[4].SetActive(true);
+                    ClickNum++;
+                    break;
+                case 27:
+                    TutorialText.text = "자신의 유닛을 클릭하고 공격 버튼을 클릭하고 적 유닛을 클릭해보세요.";
+                    UnitSecond = true;
+                    ButtonLimit = true;
+                    ClickNum++;
+                    break;
+                case 29:
+                    TutorialText.text = "이번에는 구역 점령과 디시전에 대해서 알려드리겠습니다.";
+                    ClickNum++;
+                    break;
+                case 30:
+                    TutorialText.text = "턴 종료 버튼을 눌러보세요.";
+                    ButtonLimit = true;
+                    TurnNext = true;
+                    break;
+                case 32:
+                    TutorialText.text = "가운데 나타난 디시전 버튼을 클릭해보세요.";
+                    Dicision = true;
+                    ClickNum++;
+                    break;
+                case 34:
+                    TutorialText.text = "초록색 버튼 중 하나를 골라 선택하세요. 빨간색 버튼은 선택할 수 없는 항목입니다.";
+                    ClickNum++;
+                    break;
+                case 36:
+                    TutorialText.text = "이것으로 튜토리얼을 마치겠습니다. 무운을 빕니다.";
+                    UIs[11].SetActive(false);
+                    UIs[22].SetActive(false);
+                    break;
             }
         }
     }
--- Anarchy_mobile/Assets/Scripts/4_Tutorial/Tutorial_Btn.cs
+++ Anarchy_mobile/Assets/Scripts/4_Tutorial/Tutorial_Btn.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class Tutorial_Btn : MonoBehaviour
 {
     public TutorialMain tutorialMain;
+    public Color panelColor;
+    int turnnum = 0;
 
     public void UnitBtn()
     {
-        if (tutorialMain.ButtonLimit)
+        if (tutorialMain.UButtonLimit)
         {
             if (tutorialMain.Unit2nd)
             {
@@ -32,6 +34,7 @@ public class Tutorial_Btn : MonoBehaviour
                 tutorialMain.UIs[12].transform.SetAsLastSibling();
                 tutorialMain.UIs[10].transform.SetAsLastSibling();
                 tutorialMain.ClickNum++;
+                tutorialMain.UButtonLimit = false;
             }
         }
         
@@ -44,6 +47,7 @@ public class Tutorial_Btn : MonoBehaviour
                 tutorialMain.UIs[12].SetActive(false);
                 tutorialMain.Units[1].SetActive(true);
                 tutorialMain.ClickNum++;
+                tutorialMain.Money.text = "800";
                 tutorialMain.TutorialText.text = "이 유닛은 원거리 유닛입니다. 공격에 특화된 유닛으로 공격력이 높습니다.";
         }
     }
@@ -54,13 +58,14 @@ public class Tutorial_Btn : MonoBehaviour
             tutorialMain.UIs[11].SetActive(false);
             tutorialMain.UIs[12].SetActive(false);
             tutorialMain.Units[2].SetActive(true);
+            tutorialMain.Money.text = "550";
             tutorialMain.TutorialText.text = "이 유닛은 영웅 유닛입니다. 공방능력이 모두 좋은 유닛으로 가장 강력한 유닛입니다.";
             tutorialMain.ClickNum++;
         }
     }
     public void BuildBtn()
     {
-        if (tutorialMain.ButtonLimit)
+        if (tutorialMain.BButtonLimit)
         {
             tutorialMain.TutorialText.text = "건물 생산창입니다. 첫번째 버튼을 클릭해보세요.";
             tutorialMain.UIs[9].transform.SetAsLastSibling();
@@ -70,14 +75,16 @@ public class Tutorial_Btn : MonoBehaviour
             tutorialMain.UIs[13].transform.SetAsLastSibling();
             tutorialMain.UIs[10].transform.SetAsLastSibling();
             tutorialMain.ClickNum++;
+            tutorialMain.BButtonLimit = false;
         }
-            tutorialMain.BuildBtn = true;
+
     }
     public void Build1Btn()
     {
         if (tutorialMain.ButtonLimit)
         {
             tutorialMain.TutorialText.text = "이렇게 건물을 건설할 수 있습니다.";
+            tutorialMain.Money.text = "250";
             tutorialMain.Building.SetActive(true);
             tutorialMain.UIs[11].SetActive(false);
             tutorialMain.UIs[13].SetActive(false);
@@ -88,13 +95,23 @@ public class Tutorial_Btn : MonoBehaviour
     {
         if (tutorialMain.ButtonLimit)
         {
-            Debug.Log(tutorialMain.ClickNum);
-            tutorialMain.TutorialText.text = "유닛 정보창입니다. 유닛의 성능과 현재 상태를 볼 수 있고, 유닛에게 이동, 전투 명령을 내릴 수 있습니다.";
-            tutorialMain.UIs[14].SetActive(true);
-            tutorialMain.UIs[15].SetActive(true);
-            tutorialMain.ClickNum++;
+            if (tutorialMain.UnitSecond)
+            {
+                tutorialMain.TutorialText.text = "공격 버튼을 클릭해보세요.";
+                tutorialMain.UIs[14].SetActive(true);
+                tutorialMain.UIs[15].SetActive(true);
+                tutorialMain.UnitLimit = false;
+            }
+            else
+            {
+                tutorialMain.TutorialText.text = "유닛 정보창입니다. 유닛의 성능과 현재 상태를 볼 수 있고, 유닛에게 이동, 전투 명령을 내릴 수 있습니다.\n건물도 마찬가지로 클릭하면 정보창이 나타납니다.";
+                tutorialMain.UIs[14].SetActive(true);
+                tutorialMain.UIs[15].SetActive(true);
+                tutorialMain.ClickNum++;
+            }
         }
     }
+
     public void MvBtn()
     {
         if (tutorialMain.UnitLimit)
@@ -104,18 +121,30 @@ public class Tutorial_Btn : MonoBehaviour
             tutorialMain.UIs[15].SetActive(false);
             tutorialMain.UIs[11].SetActive(true);
             tutorialMain.UIs[16].SetActive(true);
+            tutorialMain.UnitLimit = false;
         }
     }
-    /*
     public void LocateBtn()
     {
         tutorialMain.TutorialText.text = "이동할 곳이 선택되었습니다. 우측 하단의 결정 버튼을 눌러주세요.";
+        panelColor = tutorialMain.UIs[17].GetComponent<Image>().color;
+        tutorialMain.UIs[17].GetComponent<Image>().color = new Color(0, 0, 255, 255f);
         tutorialMain.BtnClicked = true;
+        tutorialMain.UnitLimit = true;
     }
-    */
     public void MvokBtn()
     {
-        tutorialMain.ClickNum++;
+        if (tutorialMain.UnitLimit)
+        {
+            tutorialMain.TutorialText.text = "유닛이 이동되었습니다.\n튜토리얼 중에는 화면이 자동으로 이동하나 실제 게임에서는 구역을 클릭하여 이동하여야 합니다.";
+            tutorialMain.CameraView.position = new Vector3(-3, 6, -12);
+            tutorialMain.UIs[11].SetActive(false);
+            tutorialMain.UIs[16].SetActive(false);
+            tutorialMain.Units[0].SetActive(false);
+            tutorialMain.Units[3].SetActive(true);
+            tutorialMain.ButtonLimit = false;
+            tutorialMain.ClickNum++;
+        }
     }
     public void MinimapBtn()
     {
@@ -135,16 +164,75 @@ public class Tutorial_Btn : MonoBehaviour
     public void TurnNextBtn()
     {
         if (tutorialMain.ButtonLimit)
-            tutorialMain.TurnNextBtn = true;
+        {
+            if (tutorialMain.TurnNext)
+            {
+                switch(turnnum)
+                {
+                    case 0:
+                        tutorialMain.TutorialText.text = "1번 클릭하셨습니다. 한번 더 클릭하세요.";
+                        tutorialMain.ClickNum++;
+                        turnnum++;
+                        break;
+                    case 1:
+                        tutorialMain.TutorialText.text = "2번 클릭하셨습니다. 한번 더 클릭하세요.";
+                        turnnum++;
+                        break;
+                    case 2:
+                        tutorialMain.TutorialText.text = "3번 클릭하셨습니다. 이제 이 구역은 점령되었습니다.";
+                        tutorialMain.UIs[18].SetActive(true);
+                        tutorialMain.UIs[19].SetActive(true);
+                        tutorialMain.TurnNext = false;
+                        tutorialMain.ClickNum++;
+                        break;
+                }
+            }
+        }
     }
     public void SettingBtn()
     {
         if (tutorialMain.ButtonLimit)
             tutorialMain.SettingBtn = true;
     }
+    public void Eunit3DBtn()
+    {
+        if (tutorialMain.UnitSecond)
+        {
+            tutorialMain.TutorialText.text = "이렇게 전투를 진행할 수 있습니다.\n실제 게임에서는 체력이 모두 소진되면 유닛이 사라집니다.";
+            tutorialMain.Units[4].SetActive(false);
+            tutorialMain.UIs[14].SetActive(false);
+            tutorialMain.UIs[15].SetActive(false);
+            tutorialMain.ButtonLimit = false;
+            tutorialMain.UnitSecond = false;
+            tutorialMain.ClickNum++;
+        }
+     }
     public void AtkBtn()
     {
-
+        if (tutorialMain.UnitSecond)
+        {
+            tutorialMain.TutorialText.text = "적 유닛을 클릭해보세요.";
+        }
+    }
+    public void DisicionBtn()
+    {
+        if(tutorialMain.Dicision)
+        {
+            tutorialMain.TutorialText.text = "디시전 화면입니다.";
+            tutorialMain.UIs[21].SetActive(true);
+            tutorialMain.ClickNum++;
+        }
+    }
+    public void DisicionSltBtn()
+    {
+        tutorialMain.TutorialText.text = "디시전을 선택하셨습니다. 이 화면은 선택한 디시전의 효과를 알려주는 화면입니다.\n\n클릭해서 계속하세요.";
+        tutorialMain.UIs[21].SetActive(false);
+        tutorialMain.UIs[11].SetActive(true);
+        tutorialMain.UIs[22].SetActive(true);
+        tutorialMain.UIs[9].SetActive(true);
+        tutorialMain.UIs[9].transform.SetAsLastSibling();
+        tutorialMain.UIs[10].transform.SetAsLastSibling();
+        tutorialMain.ClickNum++;
     }
 
 }
